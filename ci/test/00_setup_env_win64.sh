695b4410643771e35f2fbc36e1e7df9098f74adf
stringintech <stringintech@gmail.com>
2026-04-27 19:05:07 +0330

    ci: focus MinGW experiment on Linux cross-build

--- ci/test/00_setup_env_win64.sh
+++ ci/test/00_setup_env_win64.sh
@@ -7,7 +7,7 @@
 export LC_ALL=C.UTF-8
 
 export CONTAINER_NAME=ci_win64
-export CI_IMAGE_NAME_TAG="mirror.gcr.io/debian:trixie"  # Check that https://packages.debian.org/trixie/g++-mingw-w64-ucrt64 can cross-compile
+export CI_IMAGE_NAME_TAG="mirror.gcr.io/debian:sid"  # Focused experiment: probe the Debian sid MinGW GCC 15 toolchain on the existing Linux cross-build path
 export HOST=x86_64-w64-mingw32ucrt
 export PACKAGES="g++-mingw-w64-ucrt64 nsis"
 export RUN_UNIT_TESTS=false
